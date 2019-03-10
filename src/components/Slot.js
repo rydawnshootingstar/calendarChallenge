c151782b61d115796c20b2cac7c8a1a1a82518b5
ryan <rydawnshootingstar@gmail.com>
2019-03-09 19:30:15 -0500

    styled appointment detail

--- src/components/Slot.js
+++ src/components/Slot.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Icon } from 'semantic-ui-react';
 
 /*
     -Timeslots have a simple class applied based on presence of a name prop
@@ -7,11 +8,18 @@ import React from 'react';
 class Slot extends React.Component{
 
     render(){
+        const {name, hour, phone} = this.props;
         return (
             <div className={this.props.name ? "slot-taken" : "slot-free"}>
-                <h3>{this.props.hour}</h3>
-                <p>{this.props.name}</p>
-                <p>{this.props.phone}</p>
+                <h3 className="slot__time">{hour}</h3>
+                <p className="slot__name">
+                    {name && <Icon style={{marginRight: '10px'}} name="user" size="small"/>}
+                    {name}
+                </p>
+                <p className="slot__phone">
+                    {phone && <Icon style={{marginRight: '10px'}} name="phone" size="small"/>}
+                    {phone}
+                </p>                
             </div>
         )
     }
